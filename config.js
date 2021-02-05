a6f7a99c2691d2b7cda45907e92f316a4e7fe248
juantd25 <juantd25@hotmail.com>
2021-02-05 07:58:29 -0500

    ok

---
+++ config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    remoteDB: process.env.REMOTE_DB || false,
+    api: {
+        port: process.env.API_PORT || 3000,
+    },
+    post: {
+        port: process.env.POST_PORT || 3002,
+    },
+    jwt: {
+        secret: process.env.JWT_SECRET || '',
+    },
+    mysql: {
+        host: process.env.MYSQL_HOST || '',
+        user: process.env.MYSQL_USER || '',
+        password: process.env.MYSQL_PASS || '',
+        database: process.env.MYSQL_DB || '',
+    },
+    mysqlService: {
+        host: process.env.MYSQL_SRV_HOST || 'localhost',
+        port: process.env.MYSQL_SRV_PORT || 3001,
+    }
+}
